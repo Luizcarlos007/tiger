42247c3bb508f8c6a3167ecf652e3df2d5c2ad3e
forcegpt <160194194+forcegpt@users.noreply.github.com>
2024-02-28 08:13:58 -0300

    Update index2.js

--- js/index2.js
+++ js/index2.js
@@ -2,7 +2,7 @@ const body = document.querySelector('body');
 const topBar = `<div class="top-bar">
 <div class="container-layout">
   <div class="top-bar-content">
-    <a href="https://luizcarlos007.github.io/principal/">
+    <a href="https://forcegpt.github.io/principal/">
       <div class='item'>
         <i class='bx bx-left-arrow-alt'></i>
         <span>
@@ -10,7 +10,7 @@ const topBar = `<div class="top-bar">
         </span>
       </div>
     </a>
-    <a href="https://luizcarlos007.github.io/principal/">
+    <a href="https://forcegpt.github.io/principal/">
       <div class='item'>
         <i class="bx bxs-home "></i>
         <span>
@@ -18,7 +18,7 @@ const topBar = `<div class="top-bar">
         </span>
       </div>
     </a>
-    <a href="https://luizcarlos007.github.io/aula/">
+    <a href="https://forcegpt.github.io/aula/">
       <div class='item '>
       <i class='bx bxs-graduation'></i>
         <span>
